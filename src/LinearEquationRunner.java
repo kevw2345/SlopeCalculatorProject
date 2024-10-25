f5c0d64a570741f9b902bf1fc1503a93be5ac2a9
BT_1N3_25 <sample@bths.edu>
2024-10-25 10:38:35 -0400

    First Commit

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,5 +1,6 @@
 public class LinearEquationRunner {
     public static void main(String[] args) {
+        //runs the program
         LinearEquationLogic logic = new LinearEquationLogic();
         logic.start();
     }
